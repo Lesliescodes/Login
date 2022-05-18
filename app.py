413242742c0229b3172ed98e7c489c16c1476d1e
Leslie Allen <lesliecodes@gmail.com>
2022-05-17 21:53:11 -0600

    fixing bugs

--- app.py
+++ app.py
@@ -5,3 +5,18 @@ password = 'slientpassword'
 
 username_input = ('username: ')
 password_input = ('password: ')
+
+if username_input == username and password_input == password:
+    print('Access granted')
+    print('Please wait')
+    time.sleep(5)
+    print('ok... Loading...')
+    time.sleep(1)
+    print('...')
+    print('Alright you have security clearance. Pulling up the secret mainframe.')
+elif username_input == username and password_input != password:
+    print('Password incorrect')
+elif username_input != username and password_input == password:
+    print('Username incorrect')
+else:
+    print('Incorrect entry please check your username and password')
